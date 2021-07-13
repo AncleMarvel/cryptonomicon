c5de723d2466a361a0dacf0b19dd40bb85b868e6
AncleMarvel <anlcemarvel@gmail.com>
2021-07-13 18:45:26 +0300

    ready graph

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@
           <div
             v-for="t in tickers"
             :key="t.name"
-            @click="sel = t"
+            @click="select(t)"
             :class="{
               'border-4': sel === t
             }"
@@ -142,10 +142,12 @@
           {{ sel.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div class="bg-purple-800 border w-10 h-24"></div>
-          <div class="bg-purple-800 border w-10 h-32"></div>
-          <div class="bg-purple-800 border w-10 h-48"></div>
-          <div class="bg-purple-800 border w-10 h-16"></div>
+          <div
+            v-for="(bar, idx) in normalizeGraph()"
+            :key="idx"
+            :style="{ height: `${bar}%` }"
+            class="bg-purple-800 border w-10"
+          ></div>
         </div>
         <button
           @click.stop="sel = null"
@@ -187,7 +189,9 @@ export default {
       ticker: "",
       tickers: [],
       sel: null,
-      apiKey: "81b05db8496581d7d2a2f7dee21dc20505c4eb8ed23072e1b9f00cb11f68a664"
+      apiKey:
+        "81b05db8496581d7d2a2f7dee21dc20505c4eb8ed23072e1b9f00cb11f68a664",
+      graph: []
     };
   },
   methods: {
@@ -196,29 +200,50 @@ export default {
         return false;
       }
 
-      const newTicker = {
+      const currentTicker = {
         name: this.ticker,
         price: "-"
       };
 
-      this.tickers.push(newTicker);
+      this.tickers.push(currentTicker);
       setInterval(async () => {
         const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=${this.apiKey}`
+          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=${this.apiKey}`
         );
         const data = await f.json();
-        // newTicker.price = data.USD; работает только во вью 2
-        if (this.tickers.find((t) => t.name === newTicker.name)) {
-          this.tickers.find((t) => t.name === newTicker.name).price =
+        // currentTicker.price = data.USD; работает только во вью 2
+        if (
+          this.tickers.find((t) => t.name === currentTicker.name) &&
+          data.USD
+        ) {
+          this.tickers.find((t) => t.name === currentTicker.name).price =
             data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
         }
-      }, 3000);
+
+        // может не быть текущего выделенного тикера, и тогда будет ругаться, что name is null
+        if (this.sel?.name === currentTicker.name) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
 
       this.ticker = "";
     },
 
     handleDelete(tForRemove) {
       this.tickers = this.tickers.filter((t) => tForRemove.name != t.name);
+    },
+
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+
+    select(ticker) {
+      this.sel = ticker;
+      this.graph = [];
     }
   }
 };
