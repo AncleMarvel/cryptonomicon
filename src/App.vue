5a4281438ad57b058f6e4d4d839252f3776ba9e9
AncleMarvel <anlcemarvel@gmail.com>
2021-07-19 10:14:21 +0300

    saving tickers to local storage

--- src/App.vue
+++ src/App.vue
@@ -210,6 +210,16 @@ export default {
     };
   },
 
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
+
   methods: {
     add(t = "") {
       if (t != "") {
@@ -227,29 +237,32 @@ export default {
           price: "-"
         };
         this.tickers.push(currentTicker);
-
-        setInterval(async () => {
-          const f = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=${this.apiKey}`
-          );
-          const data = await f.json();
-          // currentTicker.price = data.USD; работает только во вью 2
-          if (
-            this.tickers.find((t) => t.name === currentTicker.name) &&
-            data.USD
-          ) {
-            this.tickers.find((t) => t.name === currentTicker.name).price =
-              data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          }
-
-          // может не быть текущего выделенного тикера, и тогда будет ругаться, что name is null
-          if (this.sel?.name === currentTicker.name) {
-            this.graph.push(data.USD);
-          }
-        }, 5000);
-
+        localStorage.setItem(
+          "cryptonomicon-list",
+          JSON.stringify(this.tickers)
+        );
+        this.subscribeToUpdates(currentTicker.name);
         this.ticker = "";
       }
+    },
+
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${this.apiKey}`
+        );
+        const data = await f.json();
+        // currentTicker.price = data.USD; работает только во вью 2
+        if (this.tickers.find((t) => t.name === tickerName) && data.USD) {
+          this.tickers.find((t) => t.name === tickerName).price =
+            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        }
+
+        // может не быть текущего выделенного тикера, и тогда будет ругаться, что name is null
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
     },
 
     handleDelete(tForRemove) {
